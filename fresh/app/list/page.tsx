91ff851f383d946ac53e34cfeb660737535197fe
이정관 <wellsbabo@gravity.co.kr>
2023-10-18 09:24:21 +0900

    애플후레시 완성

--- fresh/app/list/page.tsx
+++ fresh/app/list/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import {useState} from "react";
+
 export default function List (){
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
     let array = [2,3,4]
@@ -7,6 +10,9 @@ export default function List (){
 
         return 10
     })
+    let[수량, 수량set] = useState([0 ,0 ,0])
+
+    // let 수량test = 0
 
     console.log(array)
     console.log(b)
@@ -20,6 +26,17 @@ export default function List (){
                         <div className="food" key={index}>
                             <img src={`/food${index}.png`} alt={data} className={"food-img"}></img>
                             <h4>{data} $40</h4>
+                            <span> {수량[index]} </span>
+                            <button onClick={() => {
+                                let copy = [...수량]
+                                copy[index]++
+                                수량set(copy)
+                            }}>+</button>
+                            <button onClick={() => {
+                                let copy = [...수량]
+                                copy[index]--
+                                수량set(copy)
+                            }}>-</button>
                         </div>
                         )
                 })
